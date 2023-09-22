ea34700c10b503ced13dd00755b4efb8df230fb3
fahadfhs <90725219+fahadfhs@users.noreply.github.com>
2023-09-22 00:54:57 -0400

    updated the DatabaseConnection class with self assignment with 4 variables required for mysql. updated the variables for the connection method in that class as well. ? was replaced to %s, cursor variable was created here as it is needed in mysql for reading the data. That completes it.

---
+++ README.md
@@ -0,0 +1,24 @@
+# Student Management System
+
+The Student Management System is a graphical desktop application developed using Python and PyQt6. 
+It is designed to streamline the management of student records in educational institutions. 
+This user-friendly system provides educators and administrators with a centralized hub to
+efficiently handle essential tasks related to student data, such as adding, searching, editing,
+and deleting student records.
+
+# Features
+### Graphical User Interface (GUI):
+The application offers an intuitive GUI with a menu bar, toolbar, and status bar for easy navigation
+and access to various functionalities.
+
+### CRUD Operations: 
+The system supports CRUD (Create, Read, Update, Delete) operations, allowing users to 
+perform actions like adding new students, searching for student records, updating student information, 
+and deleting student data.
+
+### Real-Time Updates: 
+Real-time updates ensure that changes made to student records are immediately reflected in the interface.
+
+### User-Friendly Error Handling: 
+The program includes effective error handling mechanisms, providing informative 
+pop-up messages to guide users and ensure valid actions.
--- main.py
+++ main.py
@@ -9,11 +9,15 @@ import mysql.connector
 
 
 class DatabaseConnection:
-    def __init__(self, database_file="database.db"):
-        self.database_file = database_file
+    def __init__(self, host="localhost", user="root", password="pythoncourse", database="school"):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
 
     def connect(self):
-        connection = sqlite3.connect(self.database_file)
+        connection = mysql.connector.connect(host=self.host, user=self.user,
+                                             password=self.password, database=self.database)
         return connection
 
 
@@ -88,10 +92,12 @@ class MainWindow(QMainWindow):
 
     def load_data(self):
         # creating a connection variable and connecting with the database
-        connection = DatabaseConnection().connect() # modified to use class to connect instead of repeated code
+        connection = DatabaseConnection().connect()  # modified to use class to connect instead of repeated code
         # extracting the values and storing them in result, students is name of table
-        result = connection.execute("SELECT * FROM students")
+        cursor = connection.cursor() # have to create cursor in mysql even for reading
+        cursor.execute("SELECT * FROM students") # removed result = cursor.execute(---)
         # makes sure whenever loaded, duplicate data is not added on top of existing
+        result = cursor.fetchall() # added result = fetchall
         self.table.setRowCount(0)
         for row_number, row_data in enumerate(result):
             # the function below created an empty row
@@ -187,9 +193,9 @@ class EditDialog(QDialog):  # similar to insert dialogue
         self.setLayout(layout)
 
     def update_student(self):
-        connection = DatabaseConnection().connect() # updated the code to connect using the class
+        connection = DatabaseConnection().connect()  # updated the code to connect using the class
         cursor = connection.cursor()
-        cursor.execute("UPDATE students SET name = ?, course= ?, mobile = ? WHERE id = ?",
+        cursor.execute("UPDATE students SET name = %s, course= %s, mobile = %s WHERE id = %s",  # ? ---> %s in mysql
                        (self.student_name.text(),
                         self.course_name.itemText(self.course_name.currentIndex()),
                         self.mobile.text(),
@@ -227,7 +233,7 @@ class DeleteDialog(QDialog):
 
         connection = DatabaseConnection().connect()  # updated using class
         cursor = connection.cursor()
-        cursor.execute("DELETE from students WHERE id = ?", (student_id,))
+        cursor.execute("DELETE from students WHERE id = %s", (student_id,))
         connection.commit()
         cursor.close()
         connection.close()
@@ -281,9 +287,9 @@ class InsertDialog(QDialog):
         name = self.student_name.text()
         course = self.course_name.itemText(self.course_name.currentIndex())  # item text as it is combo box
         mobile = self.mobile.text()
-        connection = DatabaseConnection().connect() # updated using class
+        connection = DatabaseConnection().connect()  # updated using class
         cursor = connection.cursor()
-        cursor.execute("INSERT INTO students(name, course, mobile) VALUES (?, ?, ?)", (name, course, mobile))
+        cursor.execute("INSERT INTO students(name, course, mobile) VALUES (%s, %s, %s)", (name, course, mobile))
         connection.commit()
         cursor.close()
         connection.close()
@@ -316,7 +322,8 @@ class SearchDialog(QDialog):
         name = self.student_name.text()
         connection = DatabaseConnection().connect()  # update the connection
         cursor = connection.cursor()
-        result = cursor.execute("SELECT * FROM students WHERE name = ?", (name,))
+        cursor.execute("SELECT * FROM students WHERE name = %s", (name,))  # updated the result variable as w previous
+        result = cursor.fetchall()
         rows = list(result)
         print(rows)
         items = main_window.table.findItems(name, Qt.MatchFlag.MatchFixedString)
